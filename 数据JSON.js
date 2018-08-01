f4dded1352d106945d4c72fd279204d1abdc58ef
zhishenh <zhishenhuo@gmail.com>
2018-08-01 10:39:03 -0700

    json format

---
+++ 数据JSON.js
@@ -0,0 +1,20 @@
+//非资讯类栏目: 专家讲座(lectures), 妈妈攻略(strategies), 交易平台(expenses)
+{
+	//_id: (string); 系统默认ID
+	category: (number); //栏目子类型
+	createDate：(number); //创建时间，单位毫秒
+	title: (string); //标题
+	content: (string);  //正文内容
+	imgs: [(string)];  //图片urls
+	numsLike: (number); //点赞
+	numsRead: (number); //浏览量
+	comments: [
+		{
+			userImg: (string); //用户头像
+			usreName: (string);  //用户名
+			numsLike: (number); //评论点赞
+			content: (string); //评论内容
+		}
+	] //评论
+
+}
