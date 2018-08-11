4d9a111037fc3e33d3807bf3c1cc3bf9decf9a3a
zhishenh <zhishenhuo@gmail.com>
2018-08-11 01:03:22 -0700

    login

--- MomsInLA/src/pages/user/user.ts
+++ MomsInLA/src/pages/user/user.ts
@@ -1,5 +1,6 @@
 import { Component, ViewChild } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
 
 /**
  * Generated class for the UserPage page.
@@ -14,15 +15,22 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'user.html',
 })
 export class UserPage {
-   @ViewChild('username') uname;
+  @ViewChild('username') uname;
 	@ViewChild('password') password;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  key: string = "username";
+  username: string = null;
+  constructor(public navCtrl: NavController, public navParams: NavParams, public storage: Storage) {
+
   }
   
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad UserPage');
+  }
+
+  ionViewWillEnter(){
+    this.storage.get(this.key).then((val)=> this.username = val);
   }
   signIn() {
   this.navCtrl.push("LoginPage");
